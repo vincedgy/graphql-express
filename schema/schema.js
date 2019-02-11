694100a7ddd49a08ddfa736e706c29efefd276f8
Vincent Dagoury ☁ <vincent.dagoury@gmail.com>
2019-02-11 22:46:18 +0100

    Adding local but also distant API calls

--- schema/schema.js
+++ schema/schema.js
@@ -1,18 +1,58 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '46', firstName: 'Samantha', age: 23 },
-];
+async function getUser(id) {
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    name: { type: GraphQLString },
+    catchPhrase: { type: GraphQLString },
+    bs: { type: GraphQLString },
+  },
+});
+
+const GeoType = new GraphQLObjectType({
+  name: 'Geo',
+  fields: {
+    lat: { type: GraphQLString },
+    lng: { type: GraphQLString },
+  },
+});
+
+const AddressType = new GraphQLObjectType({
+  name: 'Address',
+  fields: {
+    street: { type: GraphQLString },
+    suite: { type: GraphQLString },
+    city: { type: GraphQLString },
+    zipcode: { type: GraphQLString },
+    geo: { type: GeoType },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
-    firstName: { type: GraphQLString },
-    age: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    username: { type: GraphQLString },
+    email: { type: GraphQLString },
+    address: { type: AddressType },
+    phone: { type: GraphQLString },
+    website: { type: GraphQLString },
+    company: { type: CompanyType },
   },
 });
 
@@ -23,7 +63,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return getUser(args.id);
       },
     },
   },
